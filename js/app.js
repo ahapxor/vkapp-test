8eed576d180e463655846b9682bdb5f83aa2631b
Dmitry Biriukov <dmitry@1worldonline.com>
2016-07-31 05:29:24 +0300

    working in ng integration

---
+++ js/app.js
@@ -0,0 +1,71 @@
+var app = angular.module('app', ['ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+
+    // route for the home page
+        .when('/', {
+            templateUrl : 'templates/message-list.html',
+            controller  : 'app.messageListController'
+        })
+
+        // route for the about page
+        //.when('/configs', {
+        //    templateUrl : 'resources/pages/configs.html',
+        //    controller  : 'configsController'
+        //})
+
+        // route for the contact page
+        //.when('/java-deploy', {
+        //    templateUrl : 'resources/pages/java-deploy.html',
+        //    controller  : 'javaDeployController'
+        //})
+
+        .otherwise({
+            redirectTo: '/',
+            templateUrl : 'templates/message-list.html',
+            controller  : 'app.messageListController'
+        });
+});
+
+app.factory('vkSevanService', function() {
+    var vk = {
+        data: {},
+        appID: 5561099,
+        //appPermissions: 16,
+
+        init: function () {
+            VK.init({apiId: vk.appID});
+        },
+
+        getMessagesList: function(offset, count) {
+            var def = $.Defered();
+
+            VK.api('wall.get', {
+                    domain: 'sevanimals',
+                    offset: offset,
+                    count: count
+                },
+                function (r) {
+                    var resp = r.response;
+                    resp.shift();
+                    def.resolve(resp);
+                });
+
+            return def.promise();
+        }
+    };
+    vk.init();
+    return vk;
+});
+
+app.controller('app.messageListController', ['$scope', 'vkSevanService',
+    function ($scope, vkSevanService) {
+        $scope.messages = [];
+
+        vkSevanService
+            .getMessagesList(1, 25)
+            .then(function(resp) {
+                $scope.messages = resp;
+            });
+}]);
